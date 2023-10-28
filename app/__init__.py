b29e335fd0eee15e1cdecf7d2d316aeae79f2bde
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-10-28 01:56:04 -0600

    Set Up Skeleton Server

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from .config import config
 
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
