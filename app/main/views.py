321996720d483a571714f5d2a6669fd549ec0783
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-11-04 19:46:43 -0600

    Wrote HTML

--- app/main/views.py
+++ app/main/views.py
@@ -11,8 +11,10 @@ def index():
 def sky_color(color):
     return render_template('sky_color.html', color = color)
 
-@main.route('/state_level/<state>')
-def state_level(state):
+@main.route('/state_level/dummy_data', endpoint = 'dummy_data')
+def dummy_data():
+
+    state = 'DEMO_PAGE'
 
     plot = vis.dummy_plot()
 
@@ -24,3 +26,10 @@ def download_dummy_data():
     raw_data = csv_exp.generate_dummy_csv()
 
     return Response(raw_data, content_type= 'text/csv', headers={'Content-Disposition': 'attachment; filename=dummy_data.csv'})
+
+@main.route('/state_level/<state>', endpoint = 'state_data')
+def state_level(state):
+
+    plot = vis.dummy_plot()
+
+    return render_template('visualization_template.html', state = state, plot = plot)
