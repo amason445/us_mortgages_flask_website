0d38c1fcc33f9e48993768a1453da7b1741fd88d
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-11-14 18:05:19 -0700

    Wrote tests for interest rates

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -1,7 +1,10 @@
 import unittest
 from flask import current_app
 from app import create_app
+from app import models as mdl
 from app import visualizations as vis
+
+import pandas as pd
 import base64
 
 class BasicsTestCase(unittest.TestCase):
@@ -19,6 +22,26 @@ class BasicsTestCase(unittest.TestCase):
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
 
+class TestModels(unittest.TestCase):
+
+    def interest_rate_model_test(self, state_name):
+        try:
+            df = mdl.StateInterestRateSeries(state_name= state_name).return_df()
+            
+            if df is not None:
+                df.to_csv(f'tests/models/interest_rate_models/{state_name}_interest_rate.csv')
+            else:
+                print(f"Failed to fetch data for state {state_name}")
+
+        except Exception as e:
+            print(e)
+
+    def test_models(self):
+        state_list = ['AZ', 'CO', 'NM', 'UT']
+
+        for state in state_list:
+            self.interest_rate_model_test(state_name= state)
+
 class TestVisualizations(unittest.TestCase):
 
     def interest_rates_test(self, state_name):
@@ -26,7 +49,7 @@ class TestVisualizations(unittest.TestCase):
             plot = vis.state_interest_rates(state_name=state_name)
             bin_data = base64.b64decode(plot)
 
-            with open(f'tests/interest_rate_graphs/{state_name}_interest_rate.png', 'wb') as f:
+            with open(f'tests/graphs/interest_rate_graphs/{state_name}_interest_rate.png', 'wb') as f:
                 f.write(bin_data)
                 f.close()
 
