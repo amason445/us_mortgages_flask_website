1ad8299b5cdbb05eed5f0a853da3055f3f23bdbb
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-11-15 23:00:08 -0700

    Updated Design Document

--- app/models.py
+++ app/models.py
@@ -34,10 +34,6 @@ class StateLoanVolumes(CouchDBQuery):
         return df
 
 
-
-
-
-
 class StateInterestRateSeries(CouchDBQuery):
 
     request_url = Config.COUCHDB_ROOT_URL + 'state_mortgage_records_v2//_design//state_mortgages_design_//_view//average_interest_rate_per_state?group=true'
@@ -53,5 +49,19 @@ class StateInterestRateSeries(CouchDBQuery):
         df['Loan Term'] = (df['Loan Term'] // 12).astype(str) + '-year'
         df['value'] = df['value'].round(2)
         
+        return df
+    
+class StateLoanVolumes(CouchDBQuery):
+
+    request_url = Config.COUCHDB_ROOT_URL +  '//state_mortgage_records_v2//_design//state_mortgages_design_//_view//year_state_records?group=true'
+
+    def return_df(self):
+        request = requests.get(self.request_url)
+        request_data = request.json()
+        df = pd.DataFrame(request_data['rows'])
+        df[['Year', 'State']] = pd.DataFrame(df['key'].to_list(), index= df.index)
+        df = df[['Year','State', 'value']]
+        df = df[df['State'] == self.state_name]
+
         return df
 
