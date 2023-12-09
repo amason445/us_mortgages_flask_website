92cd0df736325488ddd3ed272cbdd60bd57b7453
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-12-08 19:42:29 -0700

    Added Documentation

--- load_db/load_db.py
+++ load_db/load_db.py
@@ -1,3 +1,13 @@
+"""
+This script contains the load function and scoping. It accesses the Consumer Protection Finance Bureau API below and scrapes CSVs by state and year.
+Since the data was rather larged, I used Python's multiprocessing library. So, this folder includes a worker function that process each individual CSV.
+The CSVs are converted into dictionaries and then loaded into CouchDB as JSON. Database pathing is set in a toml file called Config.toml.
+I included a template file for this toml for my privacy. It's copied from the file I set in my .gitignore but removes sensitive information.
+The process does not guarantee consistency and it is possible for individual documents to fail. Therefore, it's important to check logs and reupload if necessary.
+https://ffiec.cfpb.gov/documentation/api/data-browser/
+"""
+
+
 import logging
 logging.basicConfig(filename='load_db\\logging\\db_load.log', encoding='utf-8', level=logging.INFO, 
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -10,18 +20,22 @@ from functools import partial
 
 if __name__ == '__main__':
 
+    # these lists contain the data base scope
     years = [2022, 2021, 2020, 2019, 2018]
-
     state_abbreviations = ['CO', 'UT', 'AZ', 'NM']
     
+    # the year and state lists are packaged into tuples using a cartesian product
     state_years = list(itertools.product(years, state_abbreviations))
 
     try:
 
+        #sets the maximum number of CPUs for the multiprocessing. I subtracted two to conserve CPU resources during the load
         cpu_floor = multiprocessing.cpu_count() - 2
 
+        #this builds a CPU pool for the workers defined in the worker function
         pool = multiprocessing.Pool(processes= cpu_floor)
     
+        #this maps the workers to the CPU pool and iterates over the tuples defines by the scoping
         pool.map(func= partial(worker.worker), iterable= state_years)
 
         pool.close()
--- load_db/worker.py
+++ load_db/worker.py
@@ -1,3 +1,12 @@
+"""
+This script contains the worker function for the load process. The worker function creates a worker and assigns it to the CPU pool.
+The function accesses the API below and pulls a CSV based on year and state. It then converts the CSV into a JSON document and loads it to CouchDB.
+Each JSON document contains 1000 records from each CSV. They are loaded into CouchDB as record chunks.
+https://ffiec.cfpb.gov/documentation/api/data-browser/
+"""
+
+
+
 import logging
 
 import toml
@@ -9,26 +18,29 @@ from io import StringIO
 
 def worker(data_tuple):
 
+    # contains pathing information for the CouchDB database
     load_config = toml.load('load_db/config_load.toml')
 
+    # initializes CouchDB connection
     couch = couchdb.Server(load_config['CouchDB']['connection_string'])
-
     couch_db = couch[load_config['CouchDB']['loan_records_db']]
 
     logging.info(f'Worker initialized for: {data_tuple[0]}-{data_tuple[1]}')
 
+    # this is the API request URL. It uses the tuple as parameters.
     hmda_api_request = f'https://ffiec.cfpb.gov/v2/data-browser-api/view/csv?states={data_tuple[1]}&years={data_tuple[0]}&actions_taken=1'
 
     #logging.info(f'{hmda_api_request}')
 
     try:
+        
+        # GET request against the CFPB API
         response = requests.request('GET', hmda_api_request)
         request_csv = response.text
-
         logging.info(f'{data_tuple[0]}-{data_tuple[1]} API pull: {response.status_code}')
 
+        # reads results to CSV and converts records to a records dictionary
         results_list = []
-
         csv_reader = csv.DictReader(StringIO(request_csv))
 
         for row in csv_reader:
@@ -36,11 +48,13 @@ def worker(data_tuple):
 
         logging.info(f'{data_tuple[0]}-{data_tuple[1]} JSON conversion was successful. Records size: {len(results_list)}')
 
+        # slices records dictionaries into a list of lists where each list will contain 1000 chunks
         chunk_size = 1000
         record_chunks = [results_list[i: i + chunk_size] for i in range(0, len(results_list), chunk_size)]
 
         counter = 0
 
+        # each chunk is uploaded structured into an individual JSON document and uploaded to CouchDB
         for i in range(0,len(record_chunks)):       
             
             load_fragment = {}
